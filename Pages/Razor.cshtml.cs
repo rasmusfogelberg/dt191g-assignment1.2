5e1d51a523d90447cd987ab6c40d75055cf2eb5f
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2022-01-19 09:29:14 +0100

    Initial commit

---
+++ Pages/Razor.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_tutorial.Pages;
+
+public class RazorModel : PageModel
+{
+  private readonly ILogger<RazorModel> _logger;
+  public List<Course> Courses { get; set; }
+
+
+  public class Course
+  {
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+  }
+
+  public RazorModel(ILogger<RazorModel> logger)
+  {
+    _logger = logger;
+
+    Courses = new List<Course>
+        {
+            new Course { Id = 1, Name = "Course 1" },
+            new Course { Id = 2, Name = "Course 2" },
+            new Course { Id = 3, Name = "Course 3" }
+        };
+  }
+
+  public void OnGet()
+  {
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Razmus
+An extremely simple Razor app.
+
+## Quickstart
+
+0. Clone this repo
+1. `dotnet dev-certs https --trust`
+2. Press CTRL+F5, use .NET Core as the environment
+3. Copy the url from the terminal showing: `"Now listening on: https://localhost:{port-here}"`
